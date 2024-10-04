cf6efbf7a192f11ee8872af0f17cdabdaf4e81e6
JoaquimMoco <103006867+JoaquimMoco@users.noreply.github.com>
2024-10-04 15:46:41 -0300

    Add files via upload

---
+++ estruturas_condicao1.py
@@ -0,0 +1,10 @@
+Temperatura = 29
+def range(Temperatura):
+    if Temperatura <= 30:
+        return("A temperatura hoje está amena")
+    else:
+        return("Hoje está fazendo calor")
+print(range(Temperatura))
+
+
+
---
+++ estruturas_repeticao3.py
@@ -0,0 +1,8 @@
+entrada_idade = ""
+
+while entrada_idade !="":
+    entrada_idade = Digite um número qualquer ou 0 para sair:
+    print("Número digitado: ")
+    
+    
+    
